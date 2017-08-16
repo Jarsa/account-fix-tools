b54488dcb80c4b3f236af77e525758fd399208da
Alan Ramos <alan.ramos@jarsa.com.mx>
2017-08-16 17:41:02 -0600

    [ADD][account_invoice_currency_date_invoice] create module

---
+++ account_invoice_currency_date_invoice/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017, Jarsa Sistemas
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Account Invoice Currency Rate with Invoice Date',
+    'summary': '''
+        Validate invoices with currency rate of invoice date if date is defined
+        ''',
+    'version': '10.0.1.0.0',
+    'category': 'Account',
+    'website': 'https://www.jarsa.com.mx/',
+    'author': 'Jarsa Sistemas',
+    'license': 'AGPL-3',
+    'installable': True,
+    'depends': [
+        'account',
+    ],
+}
