f55dfbe122edf368a28367114164e800de33fbda
ougc27 <oscar.garza@jarsa.com.mx>
2018-09-06 11:13:34 -0500

    [MIG][account_tax_cash_basis_reference] - module migration

--- account_tax_cash_basis_reference/models/account_move.py
+++ account_tax_cash_basis_reference/models/account_move.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017, Jarsa Sistemas, S.A. de C.V.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -8,23 +7,30 @@ from odoo import _, api, fields, models
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    nbr_moves = fields.Float(
-        string='Tax Moves', compute="_compute_nbr_moves",
-        default=0,)
-    account_move_ids = fields.One2many(
+    nbr_tax_moves = fields.Integer(
+        string='Tax Moves', compute="_compute_nbr_tax_moves",
+        default=0, oldname='nbr_moves',)
+    tax_move_ids = fields.One2many(
         'account.move',
-        'move_id',
-        string='Account Move',
+        'invoice_move_id',
+        string='Tax Journal Entries',
+        oldname='account_move_ids',
+        help='Field to link the tax journal entries '
+             'with the invoice journal entry.',
     )
-    move_id = fields.Many2one(
+    invoice_move_id = fields.Many2one(
         'account.move',
+        string='Invoice Journal Entry',
+        oldname='move_id',
+        help='Field to link the invoice journal entry with the tax journal'
+        ' entry.'
     )
 
     @api.multi
-    def _compute_nbr_moves(self):
+    def _compute_nbr_tax_moves(self):
         for rec in self:
-            if rec.account_move_ids:
-                rec.nbr_moves = len(rec.account_move_ids)
+            if rec.tax_move_ids:
+                rec.nbr_tax_moves = len(rec.tax_move_ids)
 
     @api.multi
     def _reverse_move(self, date=None, journal_id=None):
@@ -34,20 +40,20 @@ class AccountMove(models.Model):
                 self.tax_cash_basis_rec_id.debit_move_id
                 .journal_id.type in ['bank', 'cash'] else
                 self.tax_cash_basis_rec_id.credit_move_id.date)
-        return super(AccountMove, self)._reverse_move(date, journal_id)
+        return super()._reverse_move(date, journal_id)
 
     @api.multi
-    def account_move(self):
+    def get_tax_journal_entries(self):
         return {
-            'name': _('Account Move'),
+            'name': _('Tax Journal Entries'),
             'view_type': 'form',
             'view_mode': 'tree,form',
             'res_model': 'account.move',
             'domain': [(
-                'move_id', '=', self.id)],
+                'invoice_move_id', '=', self.id)],
             'type': 'ir.actions.act_window',
             'context': {
                 'create': False,
-                'delete': False
+                'delete': False,
             }
         }
