e117495c11aa1f8102b5c036bce93eea14ac1c77
ougc27 <oscar.garza@jarsa.com.mx>
2018-09-06 11:13:34 -0500

    [MIG][account_tax_cash_basis_reference] - module migration

--- account_tax_cash_basis_reference/__manifest__.py
+++ account_tax_cash_basis_reference/__manifest__.py
@@ -1,19 +1,18 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017, Jarsa Sistemas, S.A. de C.V.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Accounting - Tax Cash Basis Move Assignation",
-    "version": "10.0.0.1.0",
-    "category": "Accounting",
-    "author": "Jarsa Sistemas",
-    "website": "https://www.jarsa.com.mx",
-    "depends": ["account_tax_cash_basis", "account_cancel"],
-    "summary": "This module assign the tax moves to the invoice moves",
-    "license": "AGPL-3",
-    "data": [
+    'name': 'Accounting - Tax Cash Basis Move Assignation',
+    'version': '11.0.1.0.0',
+    'category': 'Accounting',
+    'author': 'Jarsa Sistemas',
+    'website': 'https://www.jarsa.com.mx',
+    'depends': ['account_cancel'],
+    'summary': '''This modules add the partner to the tax
+                cash basis journal entries and link it to the invoice
+                journal entries.''',
+    'license': 'AGPL-3',
+    'data': [
         'views/account_move_view.xml',
     ],
-    "application": True,
-    "installable": True,
 }
