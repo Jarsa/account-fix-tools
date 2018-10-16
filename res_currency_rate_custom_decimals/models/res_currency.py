9009f70c8ab8231f84ba2153120d2ac0bd721fc6
Isabel Esparza <isabel.esparza@jarsa.com.mx>
2018-10-16 15:37:43 -0500

    [MIG][res_currency_rate_custom_decimals]Migration to version 12.0

--- res_currency_rate_custom_decimals/models/res_currency.py
+++ res_currency_rate_custom_decimals/models/res_currency.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017, Jarsa Sistemas, S.A. de C.V.
+# Copyright 2018, Jarsa Sistemas, S.A. de C.V.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
