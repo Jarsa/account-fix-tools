19c336e176b44a22e371e463463538c42b750325
lmgr0312 <lmiguel.gruiz@gmail.com>
2018-11-05 16:36:57 -0600

    [ADD][account_fiscalyear_close]Module

---
+++ account_fiscalyear_close/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2018 Jarsa Sistemas, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account Fiscal Year Close",
+    "summary": "Account Fiscal Year Close",
+    "version": "10.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "https://www.jarsa.com.mx/",
+    "author": "Jarsa Sistemas",
+    "license": "AGPL-3",
+    "depends": [
+        'account_accountant',
+    ],
+    "data": [
+        'wizard/account_fiscalyear_close_wizard_view.xml',
+    ],
+}
---
+++ account_fiscalyear_close/wizard/account_fiscalyear_close_wizard.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018, Jarsa Sistemas, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, fields, models
+from odoo.tools.translate import _
+
+
+class AccountFiscalYearWizard(models.TransientModel):
+    _name = 'account.fiscalyear.close.wizard'
+
+    date_start = fields.Date(
+        default=lambda self: self.get_date_start(),
+        required=True,)
+    date_end = fields.Date(
+        default=lambda self: self.get_date_end(),
+        required=True,)
+    account_id = fields.Many2one(
+        comodel_name='account.account',
+        string="Account",
+        required=True,
+    )
+    journal_id = fields.Many2one(
+        comodel_name='account.journal',
+        string="Journal",
+        domain=[('type', '=', 'general')],
+        required=True,
+    )
+
+    @api.model
+    def get_date_start(self):
+        return str(fields.date.today().year - 1) + "-01-01"
+
+    @api.model
+    def get_date_end(self):
+        return str(fields.date.today().year - 1) + "-12-31"
+
+    @api.model
+    def get_account_balances(self):
+        self._cr.execute("""
+            SELECT aml.account_id, aac.name, sum(balance) AS balance,
+            aac.user_type_id FROM account_move_line aml
+            JOIN account_account aac ON aml.account_id = aac.id
+            WHERE aml.date BETWEEN %s AND %s
+            AND aac.user_type_id IN (13, 14, 15, 16, 17)
+            GROUP BY aml.account_id, aac.name, aac.user_type_id;
+            """, (self.date_start, self.date_end)
+        )
+        return self._cr.dictfetchall()
+
+    @api.model
+    def prepare_am(self):
+        return {
+            'journal_id': self.journal_id.id,
+            'date': self.date_end,
+            'ref': _("%s Year-ending's Journal Entry") % (
+                fields.date.today().year - 1)
+        }
+
+    @api.model
+    def prepare_aml(self, item, move_id):
+        return {
+            'name': item['name'] + ': ' + str(
+                fields.Date.from_string(self.date_end).year),
+            'debit': abs(item['balance']) if item['balance'] < 0.0 else 0.0,
+            'credit': abs(item['balance']) if item['balance'] > 0.0 else 0.0,
+            'move_id': move_id.id,
+            'account_id': item['account_id'],
+        }
+
+    @api.multi
+    def get_years_ending_entry(self):
+        self.ensure_one()
+        am_obj = self.env['account.move']
+        aml_obj = self.env['account.move.line']
+        move = am_obj.create(self.prepare_am())
+        data = self.get_account_balances()
+        for item in data:
+            root_aml = aml_obj.with_context(check_move_validity=False).create(
+                self.prepare_aml(item, move))
+            root_aml.copy({
+                'account_id': self.account_id.id,
+                'debit': root_aml.credit,
+                'credit': root_aml.debit,
+            })
+        move.post()
+        return {
+            'name': _('Year Endings Account Move(s)'),
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'account.move',
+            'res_id': move.id,
+            'type': 'ir.actions.act_window',
+            'context': {
+                'create': False,
+                'delete': False,
+            }
+        }
