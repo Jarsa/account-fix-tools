67b8f10fc1ab93a6e17313b3488f4eb5cf2bf2fa
ougc27 <oscar.garza@jarsa.com.mx>
2018-11-14 14:39:20 -0600

    [MIG][account_tax_cash_basis_reference] - Module migration from V11.0

--- account_tax_cash_basis_reference/models/account_move.py
+++ account_tax_cash_basis_reference/models/account_move.py
@@ -7,53 +7,29 @@ from odoo import _, api, fields, models
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    nbr_tax_moves = fields.Integer(
-        string='Tax Moves', compute="_compute_nbr_tax_moves",
-        default=0, oldname='nbr_moves',)
-    tax_move_ids = fields.One2many(
+    tax_cash_basis_move_ids = fields.One2many(
         'account.move',
-        'invoice_move_id',
-        string='Tax Journal Entries',
-        oldname='account_move_ids',
-        help='Field to link the tax journal entries '
-             'with the invoice journal entry.',
+        'origin_move_id',
+        string='Tax Move(s)',
     )
-    invoice_move_id = fields.Many2one(
+    origin_move_id = fields.Many2one(
         'account.move',
-        string='Invoice Journal Entry',
-        oldname='move_id',
-        help='Field to link the invoice journal entry with the tax journal'
-        ' entry.'
+        string='Origin Move',
+        help='Original Journal Entry from which this move was created.',
     )
 
     @api.multi
-    def _compute_nbr_tax_moves(self):
-        for rec in self:
-            if rec.tax_move_ids:
-                rec.nbr_tax_moves = len(rec.tax_move_ids)
-
-    @api.multi
-    def _reverse_move(self, date=None, journal_id=None):
-        if self.tax_cash_basis_rec_id:
-            date = (
-                self.tax_cash_basis_rec_id.debit_move_id.date if
-                self.tax_cash_basis_rec_id.debit_move_id
-                .journal_id.type in ['bank', 'cash'] else
-                self.tax_cash_basis_rec_id.credit_move_id.date)
-        return super()._reverse_move(date, journal_id)
-
-    @api.multi
-    def get_tax_journal_entries(self):
+    def action_tax_cash_basis_moves(self):
         return {
-            'name': _('Tax Journal Entries'),
+            'name': _('Tax Move(s)'),
             'view_type': 'form',
             'view_mode': 'tree,form',
             'res_model': 'account.move',
             'domain': [(
-                'invoice_move_id', '=', self.id)],
+                'origin_move_id', '=', self.id)],
             'type': 'ir.actions.act_window',
             'context': {
                 'create': False,
-                'delete': False,
+                'delete': False
             }
         }
--- account_tax_cash_basis_reference/models/account_partial_reconcile.py
+++ account_tax_cash_basis_reference/models/account_partial_reconcile.py
@@ -1,33 +1,24 @@
 # Copyright 2017, Jarsa Sistemas, S.A. de C.V.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import models
 
 
 class AccountPartialReconcileCashBasis(models.Model):
     _inherit = 'account.partial.reconcile'
 
-    @api.model
-    def create(self, vals):
-        """ Originally Odoo does not write the partner in the account move lines
-        of the cash journal entry, to show this moves in the DIOT we need
-        set the invoice partner on that amls, also the method links the
-        cash basis journal entry to the invoice journal entry.
-
-        :param vals: dictionary of fields to create the record
-        :return: object.
-            A recordset with the new record created.
-        """
-        res = super(AccountPartialReconcileCashBasis, self).create(vals)
+    def create_tax_cash_basis_entry(self, percentage_before_rec):
+        res = super().create_tax_cash_basis_entry(percentage_before_rec)
         move_tax = self.env['account.move'].search(
-            [('tax_cash_basis_rec_id', '=', res.id)])
+            [('tax_cash_basis_rec_id', '=', self.id)])
 
-        debit_move = res.debit_move_id
-        credit_move = res.credit_move_id
+        debit_move = self.debit_move_id
+        credit_move = self.credit_move_id
+
+        # Customer Invoices
         if (debit_move.journal_id.type == 'sale' and
                 credit_move.journal_id.type in ['bank', 'cash']):
             origin_move = debit_move.move_id
-            move_date = credit_move.date
         # Refunds Validation, we takes the latest date
         elif (debit_move.journal_id.type in ['sale', 'purchase'] and
                 credit_move.journal_id.type in ['sale', 'purchase']):
@@ -35,30 +26,20 @@ class AccountPartialReconcileCashBasis(models.Model):
                 debit_move.move_id if
                 debit_move.invoice_id.type in ['in_invoice', 'out_invoice']
                 else credit_move.move_id)
-            debit_date = fields.Date.from_string(debit_move.date)
-            credit_date = fields.Date.from_string(credit_move.date)
-            move_date = (
-                fields.Date.to_string(debit_date) if
-                debit_date > credit_date else
-                fields.Date.to_string(credit_date))
+        # Supplier Invoices
         else:
             if credit_move.invoice_id:
                 origin_move = credit_move.move_id
-                move_date = debit_move.date
             else:
                 origin_move = debit_move.move_id
-                move_date = credit_move.date
-
         move_tax.button_cancel()
         move_tax.write({
             'partner_id': origin_move.partner_id.id,
-            'date': move_date,
-            'invoice_move_id': origin_move.id,
+            'origin_move_id': origin_move.id,
             'ref': origin_move.name,
         })
         move_tax.line_ids.write({
             'partner_id': origin_move.partner_id.id,
-            'date': move_date,
         })
         move_tax.post()
         return res
