85637efe65dfa9c01961c7f213d46c5e75c7d018
ougc27 <oscar.garza@jarsa.com.mx>
2019-01-04 11:55:31 -0600

    [ADD][account_fiscalyar_close_analytic] - Split account balances by analytic accounts

---
+++ account_fiscalyear_close_analytic/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# © 2018 Jarsa Sistemas, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account Fiscal Year Close Analytic",
+    "summary": "Account Fiscal Year Close with Analytic Accounts",
+    "version": "10.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "https://www.jarsa.com.mx/",
+    "author": "Jarsa Sistemas",
+    "license": "AGPL-3",
+    "depends": [
+        'account_fiscalyear_close',
+    ],
+}
---
+++ account_fiscalyear_close_analytic/wizard/account_fiscalyear_close_wizard.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018, Jarsa Sistemas, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+
+class AccountFiscalYearWizard(models.TransientModel):
+    _inherit = 'account.fiscalyear.close.wizard'
+
+    @api.model
+    def _get_account_balances_query(self):
+        query, params = super(
+            AccountFiscalYearWizard, self)._get_account_balances_query()
+        query = """
+            SELECT aml.account_id, aac.name, sum(balance) AS balance,
+                   aac.user_type_id, aml.analytic_account_id
+            FROM account_move_line aml
+            JOIN account_account aac ON aml.account_id = aac.id
+            WHERE aml.date BETWEEN %s AND %s
+            AND aac.user_type_id IN (12, 13, 14, 15, 16, 17)
+            AND aml.company_id = %s
+            GROUP BY aml.account_id, aac.name, aac.user_type_id,
+                aml.analytic_account_id;
+            """
+        return query, params
+
+    @api.model
+    def prepare_aml(self, item, move_id):
+        res = super(AccountFiscalYearWizard, self).prepare_aml(item, move_id)
+        res['analytic_account_id'] = item['analytic_account_id']
+        return res
