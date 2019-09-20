99b0f8ba8d2b5c15d29911ab884add192992ce23
lmgr0312 <lmiguel.gruiz@gmail.com>
2019-09-20 16:41:23 -0500

    [ADD][Account Invoice Fix Number]

---
+++ account_invoice_fix_number/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2019, Jarsa Sistemas, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Accounting - Invoice Fix Number",
+    "version": "12.0.0.1.0",
+    "category": "Accounting",
+    "author": "Jarsa Sistemas",
+    "website": "https://www.jarsa.com.mx",
+    "depends": ["account"],
+    "summary": "This module assign a fix number based in the invoice number",
+    "license": "AGPL-3",
+    "data": [
+        'views/account_invoice_view.xml',
+    ],
+    "installable": True,
+}
