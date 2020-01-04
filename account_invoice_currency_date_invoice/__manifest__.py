361bbd5f304ba452e834daf4ec5ab192596ae2ee
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2020-01-04 15:13:09 -0600

    [MIG] account_invoice_currency_date_invoice: changes required to work in V12

--- account_invoice_currency_date_invoice/__manifest__.py
+++ account_invoice_currency_date_invoice/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017, Jarsa Sistemas
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
@@ -6,7 +5,7 @@
     'summary': '''
         Validate invoices with currency rate of invoice date if date is defined
         ''',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Account',
     'website': 'https://www.jarsa.com.mx/',
     'author': 'Jarsa Sistemas',
--- account_invoice_currency_date_invoice/models/account_invoice.py
+++ account_invoice_currency_date_invoice/models/account_invoice.py
@@ -1,22 +1,11 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017, Jarsa Sistemas
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models
+from odoo import models
 
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    @api.multi
-    def compute_invoice_totals(self, company_currency, invoice_move_lines):
-        """This method allow to  compute exchange rate based in invoice_date field
-        not from date field."""
-        for rec in self:
-            date = rec.date
-            self.date = self.date_invoice
-            total, total_currency, invoice_move_lines = super(
-                AccountInvoice, self).compute_invoice_totals(
-                    company_currency, invoice_move_lines)
-            rec.date = date
-        return total, total_currency, invoice_move_lines
+    def _get_currency_rate_date(self):
+        return self.date_invoice
