bd254136a7e2543a7a582640550bfa9e7b4cc3e1
Jesús Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2021-06-12 10:31:04 -0500

    [ADD] account_aged_by_currency_report: show currency in search view of account reports

---
+++ account_aged_by_currency_report/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021, Jarsa
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Account Aged Reports by Currency',
+    'summary': 'Allow to see aged balance reports by currency',
+    'version': '14.0.1.0.0',
+    'category': 'Accounting',
+    'website': 'https://www.jarsa.com.mx/',
+    'author': 'Jarsa',
+    'license': 'LGPL-3',
+    'depends': [
+        'account_reports',
+    ],
+    'data': [
+        'views/search_template_view.xml',
+    ],
+}
