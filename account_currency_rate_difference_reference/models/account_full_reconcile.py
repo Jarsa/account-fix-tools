196ac33fce8fc0c091cff17f742df8efc0e31bcf
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com>
2023-05-21 14:05:55 +0000

    [FIX] account_currency_rate_difference_reference: correct use of account.move instead of account.move.line to write the reference

--- account_currency_rate_difference_reference/models/account_full_reconcile.py
+++ account_currency_rate_difference_reference/models/account_full_reconcile.py
@@ -5,7 +5,7 @@ from odoo import api, models
 
 
 class AccountFullReconcile(models.Model):
-    _inherit = 'account.full.reconcile'
+    _inherit = "account.full.reconcile"
 
     @api.model
     def create(self, vals):
@@ -14,9 +14,9 @@ class AccountFullReconcile(models.Model):
             return res
         invoice_moves = res.reconciled_line_ids.filtered(
             lambda l: l.account_id.reconcile and l.journal_id.type in
-            ['sale', 'purchase'] and l.move_id.move_type in
-            ['in_invoice', 'out_invoice'])
+            ["sale", "purchase"] and l.move_id.move_type in
+            ["in_invoice", "out_invoice"]).mapped("move_id")
         invoice_moves.write({
-            'exchange_move_id': res.exchange_move_id.id,
+            "exchange_move_id": res.exchange_move_id.id,
         })
         return res
--- account_currency_rate_difference_reference/models/account_move.py
+++ account_currency_rate_difference_reference/models/account_move.py
@@ -5,23 +5,23 @@ from odoo import _, fields, models
 
 
 class AccountMove(models.Model):
-    _inherit = 'account.move'
+    _inherit = "account.move"
 
     exchange_move_id = fields.Many2one(
-        'account.move',
-        string='Currency Exchange Difference Move',
+        "account.move",
+        string="Currency Exchange Difference Move",
     )
 
     def action_currency_rate_diference(self):
         return {
-            'name': _('Currency Exchange Difference Move'),
-            'view_type': 'form',
-            'view_mode': 'form',
-            'res_model': 'account.move',
-            'res_id': self.exchange_move_id.id,
-            'type': 'ir.actions.act_window',
-            'context': {
-                'create': False,
-                'delete': False
+            "name": _("Currency Exchange Difference Move"),
+            "view_type": "form",
+            "view_mode": "form",
+            "res_model": "account.move",
+            "res_id": self.exchange_move_id.id,
+            "type": "ir.actions.act_window",
+            "context": {
+                "create": False,
+                "delete": False
             }
         }
